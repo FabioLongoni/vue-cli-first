ab156dd4df1103bd4c1b55b1e67a8c03cd873dbb
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-02 15:22:30 +0200

    add-bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'heroContent',
+  }
+</script>
+
+<style scoped lang="scss">
+  div {
+    margin: 0 auto;
+    max-width: 100%;
+    height: 500px;
+    background-image: url(https://wallpaperaccess.com/full/221119.jpg);
+    background-size: cover;
+    background-position: center;
+  }
+</style>
